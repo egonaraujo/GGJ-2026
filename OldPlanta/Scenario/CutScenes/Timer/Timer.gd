31c25d22ab66503b3ad9c92ef267823304ce2e67
Egon Araujo <dev.egon.araujo@gmail.com>
2026-01-27 22:40:44 -0300

    Moved planta to separate folder, sterted with the workstation scene|

---
+++ OldPlanta/Scenario/CutScenes/Timer/Timer.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+@export var next: PackedScene
+@export var delay: float = 4
+
+var delayed_time = 0
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_process(true)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	delayed_time = delayed_time + delta
+	if delayed_time > delay:
+		get_tree().change_scene_to_packed(next)
+	
+	var per_cent = 1-((delay-delayed_time)/delay)
+	var finalY = -2600*per_cent
+
+	$BG01.set_position(Vector2(0, finalY))
+	$BG04.set_position(Vector2(0, finalY))
+	$BG02.set_position(Vector2(0, finalY))
